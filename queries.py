1942ef29dfa50e5332b21299b72f96f5f4c7548a
Roy Gitahi <roygitahi0@gmail.com>
2026-07-31 11:09:16 +0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,44 +1,44 @@
-# from app import app
-# from models import (
-#     User,
-#     Course,
-#     AssignmentSubmission,
-#     UserProfile,
-# )
+from app import app
+from models import (
+    User,
+    Course,
+    AssignmentSubmission,
+    UserProfile,
+)
 
-# with app.app_context():
-#     print("\n---All users--- ")
-#     users = User.query.all()
-#     for user in users:
-#         print(f"{user.id} {user.full_name} {user.email} {user.role}")
+with app.app_context():
+    print("\n---All users--- ")
+    users = User.query.all()
+    for user in users:
+        print(f"{user.id} {user.full_name} {user.email} {user.role}")
 
-#     print("\n---All courses with instructors ---")
-#     courses = Course.query.all()
-#     for course in courses:
-#         print(f"{course.title} Instructor: {course.tm_user.full_name}")
+    print("\n---All courses with instructors ---")
+    courses = Course.query.all()
+    for course in courses:
+        print(f"{course.title} Instructor: {course.tm_user.full_name}")
 
-#     print("\n---Students enrolled in Python Basics ---")
-#     course = Course.query.filter_by(title="Python Basics").first()
-#     if course:
-#         for enrollment in course.enrollments:
-#             print(f"{enrollment.student.full_name}")
+    print("\n---Students enrolled in Python Basics ---")
+    course = Course.query.filter_by(title="Python Basics").first()
+    if course:
+        for enrollment in course.enrollments:
+            print(f"{enrollment.student.full_name}")
 
-#     print("\n---All assignment submissions---")
-#     submissions = AssignmentSubmission.query.all()
-#     for sub in submissions:
-#         print(
-#             f"{sub.student.full_name} "
-#             f"{sub.assignment.title} "
-#             f"score={sub.score} status={sub.status}"
-#         )
+    print("\n---All assignment submissions---")
+    submissions = AssignmentSubmission.query.all()
+    for sub in submissions:
+        print(
+            f"{sub.student.full_name} "
+            f"{sub.assignment.title} "
+            f"score={sub.score} status={sub.status}"
+        )
 
-#     print("\n---Users with profiles---")
-#     profiles = UserProfile.query.all()
-#     for profile in profiles:
-#         print(f"{profile.user.full_name} {profile.experience_level}")
+    print("\n---Users with profiles---")
+    profiles = UserProfile.query.all()
+    for profile in profiles:
+        print(f"{profile.user.full_name} {profile.experience_level}")
 
-#     print("\n---All students---")
-#     students = User.query.filter_by(role="student").all()
-#     for student in students:
-#         print(student.full_name)
+    print("\n---All students---")
+    students = User.query.filter_by(role="student").all()
+    for student in students:
+        print(student.full_name)
 
